f5ebaa0c999200451caeae73f50a8ad6b17bf7db
Chris <68771917+Byeongju-Kong@users.noreply.github.com>
2022-09-13 22:24:12 +0900

    Thread
    * test: 학습 테스트 작성
    
    * feat: 1단계 - 동시성 이슈 확인하기
    
    * feat: 2단계 - WAS에 스레드 설정하기

--- thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
+++ thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
@@ -1,13 +1,12 @@
 package concurrency.stage0;
 
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 스레드 풀은 무엇이고 어떻게 동작할까?
@@ -31,11 +30,22 @@ class ThreadPoolsTest {
         executor.submit(logWithSleep("hello fixed thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
-        final int expectedQueueSize = 0;
+        final int expectedPoolSize = 2;
+        final int expectedQueueSize = 1;
 
-        assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
-        assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
+        assertThat(executor.getPoolSize()).isEqualTo(expectedPoolSize);
+        assertThat(executor.getQueue().size()).isEqualTo(expectedQueueSize);
+
+        /**
+         * Thread의 개수를 2로 설정해두었음. 따라서 CorePoolSize는 2
+         * executor가 submit하는 task의 개수는 3개임
+         * 따라서, 1개의 일은 당장 처리하지 못함
+         * 여기서, 새로운 Thread를 생성하는 것이 아니라, workQueue에 Runnable 객체를 실행 대기 시켜둠
+         * 그럼, 언제 새로운 Thread를 생성하나? Queue가 가득찼을 떄임.
+         * 그런데, Queue의 size를 설정해주지 않으면 size는 Integer.MAX_VALUE가 지정됨
+         * 따라서, 거의 새로운 Thread가 생성될 일이 없음.
+         * 새로운 Thread를 생성하게 함으로써 Task를 진행하도록 하려면 workQueue의 size를 지정해줘야함.
+         */
     }
 
     @Test
@@ -46,7 +56,7 @@ class ThreadPoolsTest {
         executor.submit(logWithSleep("hello cached thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
+        final int expectedPoolSize = 3;
         final int expectedQueueSize = 0;
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
