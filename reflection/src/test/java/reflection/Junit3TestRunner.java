00dd7bb7bfa557325774592f149917d866550a12
Chris <68771917+Byeongju-Kong@users.noreply.github.com>
2022-09-16 17:20:23 +0900

    reflection

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,6 +8,9 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test object = new Junit3Test();
+        Method method = clazz.getDeclaredMethod("test1");
+        method.invoke(object);
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
     }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,6 +15,12 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(type -> log.info(type.getName()));
+        reflections.getTypesAnnotatedWith(Service.class)
+                .forEach(type -> log.info(type.getName()));
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .forEach(type -> log.info(type.getName()));
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
