cb69e0d612674b1f428e9808e73dc437c804fda6
Chris <bjuuuu98@gmail.com>
2022-09-29 11:41:30 +0900

    feat: stage4

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.di.stage3.context.NoSuchBeanDefinitionException;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +15,57 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        Set<Object> beans = classes.stream()
+                .map(this::create)
+                .collect(Collectors.toSet());
+        beans.forEach(bean -> injectDependency(bean, beans));
+        this.beans = beans;
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
+    }
+
+    private Object create(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+
+    private void injectDependency(Object bean, Set<Object> beans) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                inject(bean, beans, field);
+            }
+        }
+    }
+
+    private void inject(Object bean, Set<Object> beans, Field field) {
+        for (Object injectedBean : beans) {
+            if (field.getType().isInstance(injectedBean)) {
+                field.setAccessible(true);
+                try {
+                    field.set(bean, injectedBean);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                field.setAccessible(false);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().equals(aClass))
+                .findAny()
+                .orElseThrow(NoSuchBeanDefinitionException::new);
     }
 }
